0d8e4e84daff8a93224a7a6fbe56c8ab52a1ab4f
Mehmet Ali Özdemir <malizdmr011@gmail.com>
2023-12-13 02:59:45 +0300

    backup 3
    your heap

--- Car Trip/src/Heap.java
+++ Car Trip/src/Heap.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class Heap {
     private ArrayList<Song> songs;
-    private HashSet<Integer> removedSongs;
+    private HashMap<Integer, Integer> songsSet;
     private int size;
     private boolean type;
     private String category;
@@ -14,14 +14,15 @@ public class Heap {
         this.size = 0;
         this.type = type;
         this.category = category;
-        this.removedSongs = new HashSet<Integer>();
+        this.songsSet = new HashMap<Integer, Integer>();
     }
 
     public Heap(boolean type, String category, Song[] items) {
         this(type, category);
         for (Song item: items) {
-            songs.add(item);
             size++;
+            songs.add(item);
+            songsSet.put(item.getId(), size);
         }
         buildHeap();
     }
@@ -31,54 +32,36 @@ public class Heap {
     }
 
     public void add(Song song) {
+        if (song == null) return;
+        if (songsSet.containsKey(song.getId())) return;
         size++;
-        if (removedSongs.contains(song.getId())) {
-            removedSongs.remove(song.getId());
-        }
         songs.add(song);
+        songsSet.put(song.getId(), size);
         int hole = size;
         
         while (hole > 1 && (type ? song.compareTo(songs.get(hole/2), category) > 0:song.compareTo(songs.get(hole/2), category) < 0)) {
             Song parent = songs.get(hole/2);
             songs.set(hole / 2, song);
             songs.set(hole, parent);
+            songsSet.put(parent.getId(), hole);
+            songsSet.put(song.getId(), hole / 2);
             hole = hole / 2;
         }
     }
     
     public Song peek() {
         if (size == 0) return null;
-        Song root = songs.get(1);
-        while (removedSongs.contains(root.getId())) {
-            songs.set(1, songs.get(size));
-            songs.remove(size);
-            size--;
-            removedSongs.remove(root.getId());
-            if (size > 1)
-                percolateDown(1);
-            if (size == 0)
-                return null;
-            root = songs.get(1);
-        }
         return songs.get(1);
     }
 
     public Song pop() {
         if (size == 0) return null;
+
         Song root = songs.get(1);
-        while (removedSongs.contains(root.getId())) {
-            songs.set(1, songs.get(size));
-            songs.remove(size);
-            size--;
-            removedSongs.remove(root.getId());
-            if (size > 1)
-                percolateDown(1);
-            if (size == 0)
-                return null;
-            root = songs.get(1);
-        }
         songs.set(1, songs.get(size));
+        songsSet.put(songs.get(1).getId(), 1);
         songs.remove(size);
+        songsSet.remove(root.getId());
         size--;
         if (size > 1)
             percolateDown(1);
@@ -97,7 +80,26 @@ public class Heap {
     }
 
     public void remove(int songId) {
-        removedSongs.add(songId);
+        if (songsSet.containsKey(songId)) return;
+        
+        int index = songsSet.get(songId);
+        if (index == size) {
+            songs.remove(size);
+            size--;
+            songsSet.remove(songId);
+            return;
+        }
+        Song last = songs.get(size);
+        songs.set(index, last);
+        songsSet.put(last.getId(), index);
+        songs.remove(size);
+        size--;
+        songsSet.remove(songId);
+        
+        if (size > 1) {
+            percolateDown(index);
+            percolateUp(index);
+        }
     }
 
     public void percolateDown(int hole) {
@@ -119,5 +121,16 @@ public class Heap {
             hole = child;
         }
         songs.set(hole, temp);
+    }
+    private void percolateUp(int hole) {
+        Song temp = songs.get(hole);
+        while (hole > 1 && (type ? temp.compareTo(songs.get(hole / 2), category) > 0 : temp.compareTo(songs.get(hole / 2), category) < 0)) {
+            Song parent = songs.get(hole / 2);
+            songs.set(hole / 2, temp);
+            songs.set(hole, parent);
+            songsSet.put(parent.getId(), hole);
+            songsSet.put(temp.getId(), hole / 2);
+            hole = hole / 2;
+        }
     }
 }
